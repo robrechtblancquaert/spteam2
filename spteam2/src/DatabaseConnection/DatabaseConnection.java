4ddc285ed1ba24434c1d15d3645a680052d397e2
TomVdbEhb <tom.van.der.beken1@student.ehb.be>
2017-10-29 15:25:11 +0100

    Klasse TrainingOverviewWindow en DatabaseConnection aangemaakt

---
+++ spteam2/src/DatabaseConnection/DatabaseConnection.java
@@ -0,0 +1,13 @@
+package DatabaseConnection;
+
+public class DatabaseConnection {
+
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ spteam2/src/TrainingOverviewWindow/TrainingOverviewWindow.java
@@ -0,0 +1,12 @@
+package TrainingOverviewWindow;
+
+public class TrainingOverviewWindow {
+
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
