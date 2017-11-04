5591d5e1fce59671ec23da6665830d914bd05d86
TomVdbEhb <tom.van.der.beken1@student.ehb.be>
2017-11-04 12:13:07 +0100

    getters, setters en add toegevoegd aan trainingOverviewWindow

--- spteam2/src/TrainingOverviewWindow/TrainingOverviewWindow.java
+++ spteam2/src/TrainingOverviewWindow/TrainingOverviewWindow.java
@@ -1,11 +1,37 @@
 package TrainingOverviewWindow;
 
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class TrainingOverviewWindow {
 
+	ArrayList<String> trainings = new ArrayList<>();
 	
+	public ArrayList<String> getTrainings() {
+		return trainings;
+	}
+
+	public void setTrainings(ArrayList<String> trainings) {
+		this.trainings = trainings;
+	}
+
+	
+	public void addTrainings()
+	{
+		//String trainingName;
+		System.out.println("Give the name of a training: ");
+		
+		Scanner sc = new Scanner(System.in);
+		String test=sc.nextLine();
+		
+		trainings.add(test);
+	}
+
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
 
 	}
 
