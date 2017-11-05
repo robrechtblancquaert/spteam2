7191bd173ba1a6ec2e2d96f21dc8cc1d4bf8aa51
TomVdbEhb <tom.van.der.beken1@student.ehb.be>
2017-11-05 16:32:17 +0100

    DB connectie maken

--- spteam2/src/DatabaseConnection/DatabaseConnection.java
+++ spteam2/src/DatabaseConnection/DatabaseConnection.java
@@ -1,13 +1,28 @@
 package DatabaseConnection;
 
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+
 public class DatabaseConnection {
 
-	
-	
-	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+
+		try {
+
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection conn = null;
+			conn = DriverManager.getConnection("jdbc:mysql://dt5.ehb.be/phpmyadmin","root","");
+			System.out.println("Database is connected!");
+			conn.close();
+		}
+		catch(Exception e)
+		{
+			System.out.println("Do not connect to DB - Error: " +e);
+		}
 	}
 
 }
