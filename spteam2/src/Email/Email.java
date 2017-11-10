9f34cb2fbcdb16801a1fc4352edd0d1709fe33cc
Unknown <madina.chaschiyeva@student.ehb.be>
2017-11-10 15:23:45 +0100

    Klasse Employee en Klasse Email + package aangemaakt

---
+++ spteam2/src/Email/Email.java
@@ -0,0 +1,14 @@
+package Email;
+
+import java.util.ArrayList;
+
+public class Email {
+private String to;
+private String from;
+private String body;
+private ArrayList<String> EmailAddress;
+
+public void send() {}
+}
+
+
--- spteam2/src/Employee/Employee.java
+++ spteam2/src/Employee/Employee.java
@@ -2,4 +2,7 @@ package Employee;
 
 public class Employee {
 private String name;
+private String lastName;
+private final int employeeID;
+
 }
