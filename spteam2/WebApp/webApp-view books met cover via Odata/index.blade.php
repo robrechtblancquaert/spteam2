0f59b7194a925d557d56e6806408206cd415a5f9
Unknown <tom.van.der.beken1@student.ehb.be>
2017-12-18 12:14:06 +0100

    View book aangepast met cover via oData

---
+++ spteam2/WebApp/webApp-view books met cover via Odata/index.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.app')
+
+
+@section('content')
+
+
+    <h1>Books</h1>
+    <a href="/books/create" class="btn btn-primary">Create Book</a>
+    <hr>
+    @if(count($books) > 0)
+
+        @foreach($books as $book)
+              <div class="well">
+                <div class="row">
+                    <div class="col-md-8 col-sm-8">
+                  
+                    <h3> <a href="/books/{{$book->BookID}}">{{$book->Title}}</a></h3>
+
+                </div>
+                </div>
+                </div>
+        @endforeach
+    @else
+    <p>No Books Found.</p>
+    @endif
+@endsection
+
+
+    
---
+++ spteam2/WebApp/webApp-view books met cover via Odata/show.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.app')
+
+@section('content')
+      
+
+<a href="/books"  class="btn btn-default">Go Back</a>
+        <h1><a href="https://books.google.be/books?id={!!$book->ISBN!!}">{{$book->Title}}</a></h1>
+       
+       
+        <div style="clear: left;">
+          <p style="float: left;"><img src="http://books.google.com/books/content?id={!!$book->ISBN!!}&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"  border="1px"></p>
+          <h3>Author: {{$book->Author}}</h3>
+       <br>
+       <h3>Description: <br>{!!$book->Description!!}</h3>
+       <br>
+       <h3>ISBN: {!!$book->ISBN!!}</h3>
+        </div>
+        <hr>
+      
+       <a href="/books/{{$book->BookID}}/edit">Edit</a>
+       {!!Form::open(['action' => ['BooksController@destroy',$book->BookID], 'method' => 'POST','class' => 'pull-right'])!!}
+                {{Form::hidden('_method','DELETE')}}
+                {{Form::submit('Delete',['class' => 'btn btn-danger'])}}
+        
+        {!!Form::close()!!} 
+       
+@endsection
+
+
